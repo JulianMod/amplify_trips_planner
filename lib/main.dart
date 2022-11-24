5e3688091338bb07faff254212daee604c9ef0a0
JuliaMod <julmodlinski@gmail.com>
2022-11-24 18:42:44 +0100

    add amplify api

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_trips_planner/models/ModelProvider.dart';
 import 'package:amplify_trips_planner/trips_planner_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -26,6 +29,8 @@ Future<void> main() async {
 Future<void> _configureAmplify() async {
   await Amplify.addPlugins([
     AmplifyAuthCognito(),
+    AmplifyDataStore(modelProvider: ModelProvider.instance),
+    AmplifyAPI(),
   ]);
   await Amplify.configure(amplifyconfig);
 }
